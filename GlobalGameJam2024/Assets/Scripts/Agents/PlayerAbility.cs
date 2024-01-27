462ac5de6811690501c752e1c2b688bb1c879d79
Clistenys <46068153+clistenys@users.noreply.github.com>
2024-01-26 22:02:45 -0300

    player movement

---
+++ GlobalGameJam2024/Assets/Scripts/Agents/PlayerAbility.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbility : MonoBehaviour
+{
+    public bool canUse;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        Debug.Log("teste");
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        
+    }
+}
---
+++ GlobalGameJam2024/Assets/Scripts/Agents/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovement : PlayerAbility
+{
+    public float speed = 5f; // Velocidade de movimento do jogador
+
+    public override void Start()
+    {
+        base.Start();
+
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        canUse = true;
+    }
+
+    void Update()
+    {
+        if (canUse)
+            ProcessAbility();
+    }
+
+    public void ProcessAbility()
+    {
+        // Obtém as entradas do teclado
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calcula a direção do movimento
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f);
+        movement.Normalize(); // Normaliza para garantir que a velocidade seja consistente em todas as direções
+
+        // Move o jogador
+        transform.position += movement * speed * Time.deltaTime;
+    }
+}
