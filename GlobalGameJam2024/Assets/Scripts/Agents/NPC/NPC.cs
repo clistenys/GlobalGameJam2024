f2a40b35a29f2901c0c43da59b6cf1ed7e2aeec0
Marcos Januário <mvjanuario@hotmail.com>
2024-01-27 18:27:44 -0300

    NPC e Dialogue System

---
+++ GlobalGameJam2024/Assets/Scripts/Agents/NPC/NPC.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] DialogueSystem dialogueSystem;
+    [SerializeField] Dialogue dialogue;
+    private bool isInArea, isActive, isFinished;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isInArea = false;
+        isActive = false;
+        isFinished = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInArea && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isFinished)
+            {
+                if (isActive)
+                {
+                    NextText();
+                }
+                else
+                {
+                    StartInteract();
+                }
+            }
+            else
+            {
+                dialogueSystem.HidePanel();
+                isActive = false;
+                isFinished = false;
+            }
+        }
+    }
+
+    private void StartInteract()
+    {
+        isActive = true;
+        isFinished = dialogueSystem.StartDialogue(dialogue);
+        if(isFinished)
+        {
+            dialogueSystem.HidePanel();
+            isActive = false;
+            isFinished = false;
+        }
+    }
+
+    private void NextText()
+    {
+        isFinished = dialogueSystem.NextText(dialogue);
+        if (isFinished)
+        {
+            dialogueSystem.HidePanel();
+            isActive = false;
+            isFinished = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerMovement>())
+        {
+            isInArea = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerMovement>())
+        {
+            Debug.Log("Saiu");
+            isInArea = false;
+            isActive = false;
+            isFinished = false;
+            dialogueSystem.HidePanel();
+        }
+    }
+}
